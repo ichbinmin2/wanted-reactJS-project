ff87cc17ccc90d1fdde6fb93951398481ec5b5f1
JiyeonMin <ichbinmin2@gmail.com>
2022-01-17 18:48:27 +0900

    banner slider 생성

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Carousel.module.css";
 import DATA from "./bannerData";
 import CarouselItem from "./CarouselItem/CarouselItem";
 
 function Carousel() {
-  // -g
+  const [count, setCount] = useState(0);
+  // ref
+  const slideContainer = useRef();
+
+  // Data
   const bannerList = DATA;
+  const slideLength = bannerList.length;
+
+  const rightMargin = 24;
+  const bannerInnerWidth = 1060;
+  const bannerWidth = bannerInnerWidth + rightMargin;
+
+  const width = slideLength * bannerWidth;
 
   return (
     <section className={styles.main}>
       <div className={styles.bannerWarp}>
         <div className={`${styles.slickSlider} ${styles.initialized}`}>
           <div className={styles.slickList}>
-            <div className={styles.slickTrack}>
-              <div className={styles.slickSlide}>
-                <CarouselItem info={lastBanner} key="lastCopy" />
-              </div>
+            <div
+              className={styles.slickTrack}
+              style={{
+                width: `${width}px`,
+              }}
+              ref={slideContainer}
+            >
               {bannerList.map((info) => (
                 <div className={styles.slickSlide}>
                   <CarouselItem info={info} key={info.id} />
                 </div>
               ))}
-              <div className={styles.slickSlide}>
-                <CarouselItem info={firstBanner} key="firstCopy" />
-              </div>
             </div>
           </div>
         </div>
